ffeaac394fcfe9ae49d6641acba292995a5910f6
Takahito Urai <uraitakahito@gmail.com>
2024-09-25 14:56:13 +0000

    Add a sample to bundle TypeScript scripts with rollup

--- index.ts
+++ index.ts
@@ -1,19 +1,2 @@
-function fizzbuzz(n: number): number|"Fizz"|"Buzz"|"FizzBuzz" {
-  if (n % 15 === 0) {
-    return "FizzBuzz";
-  }
-  if (n % 3 === 0) {
-    return "Fizz";
-  }
-  if (n % 5 === 0) {
-    return "Buzz";
-  }
-  return n;
-}
-
-let a: number = 1;
-console.log(fizzbuzz(a++));
-console.log(fizzbuzz(a++));
-console.log(fizzbuzz(a++));
-console.log(fizzbuzz(a++));
-console.log(fizzbuzz(a++));
+import hello from './helloworld';
+hello();
